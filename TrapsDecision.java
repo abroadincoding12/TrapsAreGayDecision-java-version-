4bc06849002afcdbacd1db4aacccfe982f9b452c
Iree McGirt <theradgamerivan@gmail.com>
2018-03-14 13:42:20 -0400

    Add files via upload

---
+++ TrapsDecision.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TrapsDecision {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int traps;
+		
+		System.out.println("Are traps gay or not gay? ");
+		traps = input.nextInt();
+
+		if(traps == gay){
+			System.out.println("Traps are gay.");
+		}
+		else if(traps == notGay){
+			System.out.println("Traps are not gay.");
+		}
+		//The code does't work. Don't try it.
+	}
+
+}
